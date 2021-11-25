f501470b8351c76023f13012b8e08c30b013f090
Barjas11 <barjas205@gmail.com>
2021-11-24 22:03:53 -0500

    initial commit

---
+++ tail.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+function tail(assertEqual){
+  return (assertEqual.splice(1, assertEqual.length - 1))
+};
+
+
+// [1, 2, 3] === [1, 2, 3] // => false
+// [1, 2, 3] == [1, 2, 3] // => false
+
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
