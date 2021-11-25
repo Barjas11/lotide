75333fd4b8bd2516c9574d5a79d48e0dc434ad5b
Barjas11 <barjas205@gmail.com>
2021-11-24 21:40:31 -0500

    initial commit

--- head.js
+++ head.js
@@ -5,5 +5,11 @@ const assertEqual = function(actual, expected) {
     console.log(`Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+
+
+function head(assertEqual){
+  return (assertEqual.splice(0, 1))}
+/* module.exports = head; */
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
