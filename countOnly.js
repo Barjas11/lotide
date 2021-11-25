d9bcddea8db8345dd23e99712229a24bd7b04d7b
Barjas11 <barjas205@gmail.com>
2021-11-25 12:36:08 -0500

    initial commit

--- countOnly.js
+++ countOnly.js
@@ -1,17 +1,46 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("😀" + `Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log("🙁" + `Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+
 const countOnly = function(allItems, itemsToCount) {
-  const results = {};
-  for (i = 0; i < allItems.length; i++) {
-    console.log(allItems[i]);
-    if (itemsToCount[allItems[i]]) {
-      if (results[allItems[i]]) {
-        results[allItems[i]] += 1;
-      } else {
-        results[allItems[i]] = 1;
+  let itemsToCountArray = Object.keys(itemsToCount)
+  const results = {}
+  for (const item of allItems) {
+    for(let countableItem of itemsToCountArray) {
+      if(item === countableItem){
+        if(typeof results[item] !== "number"){
+          results[item] = 1
+        } else if (typeof results[item] === "number"){
+          results[item] += 1
+        }
       }
     }
   }
+  console.log(results)
   return results;
+
 }
 
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
 
-module.exports = countOnly;
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
